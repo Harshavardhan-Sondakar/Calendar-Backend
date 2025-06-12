1f44502a6e26d178acd5c22a71c3823db2ac1ad3
Harshavardhan-Sondakar <hmsondakar@gmail.com>
2025-06-12 15:23:08 +0530

    first commit

---
+++ services/reminderService.js
@@ -0,0 +1,42 @@
+const { sendReminderEmail } = require('./emailService');
+const Event = require('../models/Event');
+
+const checkReminders = async () => {
+  try {
+    const now = new Date();
+    const events = await Event.find({
+      'reminders.time': { $lte: now },
+      'reminders.sent': false
+    });
+
+    for (const event of events) {
+      for (const reminder of event.reminders) {
+        if (!reminder.sent && reminder.time <= now) {
+          const emailPromises = event.peopleInvolved.map(recipient =>
+            sendReminderEmail(event, recipient)
+          );
+
+          const results = await Promise.all(emailPromises);
+          const allEmailsSent = results.every(result => result === true);
+
+          if (allEmailsSent) {
+            reminder.sent = true;
+            await event.save();
+          }
+        }
+      }
+    }
+  } catch (error) {
+    console.error('Error checking reminders:', error);
+  }
+};
+
+const startReminderService = () => {
+  checkReminders();
+  
+  setInterval(checkReminders, 20000);
+};
+
+module.exports = {
+  startReminderService
+};
